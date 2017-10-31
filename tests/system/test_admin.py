35556ca18774d0571c3a0c8744733b3419d68831
travis-ci <travis@travis-ci.org>
2017-10-31 21:09:06 +0000

    Deploy Travis CI build 31 to GitHub

--- tests/system/test_admin.py
+++ tests/system/test_admin.py
@@ -8,18 +8,7 @@ from . import user
 
 def describe_login():
 
-    def with_valid_credentials():
-        user.login('admin', 'password')
-
+    def placeholder():
         user.visit("/admin")
 
-        expect(user.browser).has_text("demo_project Administration")
-        expect(user.browser).has_text("Select a model")
-
-    def with_invalid_credentials():
-        user.login('bad-username', 'bad-password')
-
-        user.visit("/admin")
-
-        expect(user.browser.title) == "Log in | demo_project"
-        expect(user.browser).has_text("demo_project Administration")
+        expect(user.browser.title) == "Log in | Django site admin"
