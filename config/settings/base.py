8c6981846339ee2e4bf56bec6c64af8ed02ada5d
travis-ci <travis@travis-ci.org>
2017-10-31 03:29:44 +0000

    Deploy Travis CI build 26 to GitHub

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,7 +17,12 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     'rest_framework',
     'corsheaders',
+
+    'demo_project.core',
+
     'demo_project.demo_app',
+
+    'demo_project.api',
 ]
 
 MIDDLEWARE = [
