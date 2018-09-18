977b9e8e56bd8fcc819346672250cf0c1e8f1178
travis-ci <travis@travis-ci.org>
2018-09-18 19:33:48 +0000

    Deploy Travis CI build 112 to GitHub

--- demo_project/demo_app/views.py
+++ demo_project/demo_app/views.py
@@ -8,7 +8,7 @@ import log
 def current_datetime(request):
     log.debug(request)
     now = datetime.datetime.now()
-    html = ("<html><body>Hi Moxy! Welcome to demo_project again."
+    html = ("<html><body>Welcome to demo_project."
             "<br>"
             "It is now %s.</body></html>") % now
     return HttpResponse(html)
