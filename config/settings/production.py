e15aa74b02bee018bd2147b69c2871c128503045
travis-ci <travis@travis-ci.org>
2020-10-18 16:17:50 +0000

    Deploy Travis CI build 192 to GitHub

--- config/settings/production.py
+++ config/settings/production.py
@@ -30,16 +30,14 @@ DATABASES['default'] = dj_database_url.config()
 ###############################################################################
 # Caches
 
-_redis = urllib.parse.urlparse(os.environ['REDIS_URL'])
 CACHES = {
     'default': {
-        'BACKEND': 'redis_cache.RedisCache',
-        'LOCATION': f"{_redis.hostname}:{_redis.port}",
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': os.environ['REDIS_URL'],
         'OPTIONS': {
-            'PASSWORD': _redis.password,
-            'DB': 0,
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
-    },
+    }
 }
 
 ###############################################################################
