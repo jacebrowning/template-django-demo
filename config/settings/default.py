fe25630c70defacec717b649944a0633e1c349ce
travis-ci <travis@travis-ci.org>
2022-01-13 11:31:41 +0000

    Deploy Travis CI build 261 to GitHub

--- config/settings/default.py
+++ config/settings/default.py
@@ -24,7 +24,8 @@ INSTALLED_APPS = [
     "corsheaders",
     "django_extensions",
     "rest_framework",
-    # "drf_yasg",
+    # TODO: Enable when bug is fixed: https://github.com/axnsan12/drf-yasg/issues/761
+    # 'drf_yasg',
     "demo_project.api",
     "demo_project.core",
     "demo_project.demo_app",
